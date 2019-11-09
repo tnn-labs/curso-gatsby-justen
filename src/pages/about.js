37739184c001c240a102179d3898034ece998d67
theandersonn <theandersonn@gmail.com>
2019-11-09 18:17:25 -0300

    overview do gatsby-link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react"
+import { Link } from "gatsby"
 
 const AboutPage = () => (
-  <h1>About Page</h1>
+  <>
+    <h1>About Page</h1>
+
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about" activeStyle={{ color: "red" }}>About</Link>
+      </li>
+    </ul>    
+  </>
 )
 
 export default AboutPage
