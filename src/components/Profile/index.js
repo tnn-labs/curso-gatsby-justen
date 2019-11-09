f49b108317d18dc6d89508bbeb542890c5bb4ee2
theandersonn <theandersonn@gmail.com>
2019-11-09 18:40:09 -0300

    Criação do component Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matt Foo</h1>
+    <h2>Programador Fron-end</h2>
+    <p>
+      Membro do Core team da Empresa Foo, escreve sobre o mundo Fron-end e outras coisas mais.
+    </p>
+  </div>
+)
+
+export default Profile
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const AboutPage = () => (
-  <>
-    <h1>About Page</h1>
-
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about" activeStyle={{ color: "red" }}>About</Link>
-      </li>
-    </ul>    
-  </>
+  <Layout>
+    <SEO title="About" />
+    <h1>About Page</h1>  
+  </Layout>
 )
 
 export default AboutPage
