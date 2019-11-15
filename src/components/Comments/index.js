a14b54f1537bae4b6f3ca448f6630e6ed12e3fec
theandersonn <theandersonn@gmail.com>
2019-11-15 12:48:43 -0300

    Configuração do Disqus

---
+++ src/components/Comments/index.js
@@ -0,0 +1,31 @@
+import React from "react"
+import PropTypes from "prop-types"
+import ReactDisqusComments from "react-disqus-comments"
+
+import * as S from "./styled"
+
+const Comments = ({ url, title }) => {
+  const completeURL = `https://johndoe.com.br${url}`
+
+  return (
+    <S.CommentsWrapper>
+      <S.CommentsTitle>
+        Comentários
+      </S.CommentsTitle>
+      
+      <ReactDisqusComments
+        shortname="johndoe"
+        identifier={completeURL}
+        title={title}
+        url={completeURL}
+      />
+    </S.CommentsWrapper>
+  )
+}
+
+Comments.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+export default Comments
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import RecommendedPosts from "../components/RecommendPosts"
+import Comments from "../components/Comments"
 
 import * as S from "../components/Post/styled"
 
@@ -34,6 +35,7 @@ const BlogPost = ({ data, pageContext }) => {
       </S.MainContent>
 
       <RecommendedPosts next={next} previous={previous} />
+      <Comments url={post.fields.slug} title={post.frontmatter.title} />
     </Layout>
   )
 }
@@ -41,6 +43,9 @@ const BlogPost = ({ data, pageContext }) => {
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         description
