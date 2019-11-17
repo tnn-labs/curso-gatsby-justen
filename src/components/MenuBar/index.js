ab2ea37944b59f8881f3fe10630defd9a1929c58
theandersonn <theandersonn@gmail.com>
2019-11-16 21:09:57 -0300

    Criação do método para a troca de visualização

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -4,6 +4,7 @@ import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
 import { LightBulb as Light } from "styled-icons/octicons/LightBulb"
+import { ThList as List } from "styled-icons/typicons/ThList"
 
 import { Grid } from "styled-icons/boxicons-solid/Grid"
 
@@ -12,12 +13,17 @@ import * as S from "./styled"
 const MenuBar = () => {
 
   const [theme, setTheme] = useState(null)
+  const [display, setDisplay] = useState(null)
 
   const isDarkMode = theme === 'dark'
+  const isListMode = display === 'list'
 
   useEffect(() => {
     setTheme(window.__theme)
+    setDisplay(window.__display)
+    
     window.__onThemeChange = () => setTheme(window.__theme)
+    window.__onDisplayChange = () => setDisplay(window.__display)
   }, [])
 
   return (
@@ -46,8 +52,11 @@ const MenuBar = () => {
           <Light />
         </S.MenuBarItem>
         
-        <S.MenuBarItem title="Mudar visualização">
-          <Grid />
+        <S.MenuBarItem 
+          title="Mudar visualização"
+          onClick={() => window.__setPreferredDisplay(isListMode ? 'grid' : 'list')}
+        >
+          {isListMode ? <Grid /> : <List />}
         </S.MenuBarItem>
         
         <S.MenuBarItem title="Ir para o Topo">
