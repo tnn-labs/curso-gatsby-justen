de706d7b2207916957858f4cb0dac6032e68916b
theandersonn <theandersonn@gmail.com>
2019-11-17 11:56:08 -0300

    Update gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,11 +58,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `John Doe`,
+        short_name: `John Doe`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
